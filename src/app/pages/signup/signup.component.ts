f5b6622638a6b1310cd4f2452936a84099c7656f
Shida <marcellinhonkai@gmail.com>
2024-06-30 15:02:10 -0300

    thirdCommit

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,13 +6,16 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  passwordConfirm: FormControl
 }
 
+
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -22,31 +25,33 @@ interface LoginForm {
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
     constructor(
       private router: Router,
       private loginService: LoginService,
       private toastService: ToastrService
     ){
-      this.loginForm = new FormGroup ({
+      this.signupForm = new FormGroup ({
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
       })
     }
 
     submit(){
-      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-        next: () => this.toastService.success("Login success!"),
-        error: () => this.toastService.error("Login error! Try again later.")
+      this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+        next: () => this.toastService.success("Sign in success!"),
+        error: () => this.toastService.error("Sign in error! Try again later.")
       })
     }
 
     navigate(){
-      this.router.navigate(["/signup"])
+      this.router.navigate(["login"])
     }
 }
